29c372651acf0dbfb0bd4cd8102482f97ccecb78
Romulo Martins <romulomartins@MacBook-Pro-de-Romulo.local>
2020-06-06 21:43:48 -0300

    Refactoring into lib directory and passing rubocop

--- lib/doom_fire.rb
+++ lib/doom_fire.rb
@@ -1,48 +1,48 @@
+# frozen_string_literal: true
+
 require 'gosu'
 require_relative 'fire_engine'
 require_relative 'color'
 
-WIDTH = 400
+WIDTH = 420
 HEIGHT = 280
-PIXEL = 5
+PIXEL = 6
 
 FIRE_WIDTH = WIDTH / PIXEL
 FIRE_HEIGHT = HEIGHT / PIXEL
 
 class DoomFire < Gosu::Window
   def initialize
-    super WIDTH, HEIGHT, false, 1000/15
-    self.caption = "Doom Fire"
+    super WIDTH, HEIGHT, false, 1000 / 15
+    self.caption = 'Doom Fire'
 
     @palete = Color::PALETE
     @engine = FireEngine.new(FIRE_WIDTH, FIRE_HEIGHT)
     @engine.start
   end
-  
+
   def update
     @engine.calculate_propagation
   end
-  
+
   def draw
     FIRE_HEIGHT.times do |row|
       FIRE_WIDTH.times do |col|
         color = get_color row, col
         draw_rect(
-          (col * PIXEL), (row * PIXEL), 
-          PIXEL, PIXEL, 
+          (col * PIXEL), (row * PIXEL),
+          PIXEL, PIXEL,
           Gosu::Color.rgb(color[:r], color[:g], color[:b])
         )
-			end
+      end
     end
   end
-  
-  private 
-  
-  def get_color row, col
+
+  private
+
+  def get_color(row, col)
     pixel_index = col + (FIRE_WIDTH * row)
     fire_intensity = @engine.fire_pixels[pixel_index]
     @palete[fire_intensity]
   end
 end
-
-DoomFire.new.show
---
+++ main.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require_relative 'lib/doom_fire'
+
+DoomFire.new.show
